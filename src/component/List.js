d737c32739da58f1c1367610dbab98ac8b18cc25
Lokesh Gehlot <lokesh.g@chapter247.com>
2023-12-03 13:05:10 +0530

    Ft. Completed task

---
+++ src/component/List.js
@@ -0,0 +1,176 @@
+import React, { useState } from 'react';
+import './PeopleList.css';
+import Pagination from './Pagination';
+import { initialData } from '../Data';
+
+const PeopleList = () => {
+  const [data, setData] = useState(initialData);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [selectedRows, setSelectedRows] = useState([]);
+  const itemsPerPage = 10;
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  const handleDelete = (id) => {
+    const updatedData = data.filter((person) => person.id !== id);
+    setData(updatedData);
+  };
+
+  const filteredData = data.filter((person) =>
+    person.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+  const displayedData = filteredData.slice(startIndex, endIndex);
+
+  const handleEdit = (id, field) => {
+    const updatedData = data.map((person) => {
+      if (person.id === id) {
+        return { ...person, isEditing: true };
+      } else {
+        return person;
+      }
+    });
+    setData(updatedData);
+  };
+
+  const handleRowClick = (id) => {
+    setSelectedRows((prevSelectedRows) => {
+      if (prevSelectedRows.includes(id)) {
+        return prevSelectedRows.filter((rowId) => rowId !== id);
+      } else {
+        return [...prevSelectedRows, id];
+      }
+    });
+  };
+
+  const handleDeleteSelected = () => {
+    const updatedData = data.filter((person) => !selectedRows.includes(person.id));
+    setData(updatedData);
+    setSelectedRows([]);
+  };
+
+  const handleSaveEdit = (id) => {
+    const updatedData = data.map((person) => {
+      if (person.id === id) {
+        return {
+          ...person,
+          isEditing: false,
+        };
+      } else {
+        return person;
+      }
+    });
+    setData(updatedData);
+  };
+
+  const handleInputChange = (id, field, value) => {
+    const updatedData = data.map((person) => {
+      if (person.id === id) {
+        return {
+          ...person,
+          [field]: value,
+        };
+      } else {
+        return person;
+      }
+    });
+    setData(updatedData);
+  };
+
+  return (
+    <div>
+      <h1>People List</h1>
+      <input
+        type="text"
+        placeholder="Search by name"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+
+      {selectedRows.length > 0 && (
+        <button className="delete-selected" onClick={handleDeleteSelected}>
+          <i className="fa fa-trash" /> Delete Selected
+        </button>
+      )}
+
+      <table className="people-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {displayedData.map((person) => (
+            <tr key={person.id} className={person.isEditing ? 'editing' : ''}>
+              <td >
+                {person.isEditing ? (
+                  <input
+                    type="text"
+                    value={person.name}
+                    onChange={(e) =>
+                      handleInputChange(person.id, 'name', e.target.value)
+                    }
+                  />
+                ) : (
+                  person.name
+                )}
+              </td>
+              <td>
+                {person.isEditing ? (
+                  <input
+                    type="text"
+                    value={person.email}
+                    onChange={(e) =>
+                      handleInputChange(person.id, 'email', e.target.value)
+                    }
+                  />
+                ) : (
+                  person.email
+                )}
+              </td>
+              <td>
+                {person.isEditing ? (
+                  <input
+                    type="text"
+                    value={person.role}
+                    onChange={(e) =>
+                      handleInputChange(person.id, 'role', e.target.value)
+                    }
+                  />
+                ) : (
+                  person.role
+                )}
+              </td>
+              <td>
+                {person.isEditing ? (
+                  <button onClick={() => handleSaveEdit(person.id)}>Save</button>
+                ) : (
+                  <button onClick={() => handleEdit(person.id)}>Edit</button>
+                )}
+                <button onClick={() => handleDelete(person.id)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Pagination
+        totalItems={filteredData.length}
+        itemsPerPage={itemsPerPage}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
+    </div>
+  );
+};
+
+export default PeopleList;
---
+++ src/component/Pagination.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const Pagination = ({
+  totalItems,
+  itemsPerPage,
+  currentPage,
+  onPageChange,
+}) => {
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+  const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
+
+  return (
+    <div className="pagination">
+      <button onClick={() => onPageChange(1)} disabled={currentPage === 1}>
+        First
+      </button>
+      <button
+        onClick={() => onPageChange(currentPage - 1)}
+        disabled={currentPage === 1}
+      >
+        Previous
+      </button>
+      {pageNumbers.map((page) => (
+        <button
+          key={page}
+          onClick={() => onPageChange(page)}
+          className={currentPage === page ? 'active' : ''}
+        >
+          {page}
+        </button>
+      ))}
+      <button
+        onClick={() => onPageChange(currentPage + 1)}
+        disabled={currentPage === totalPages}
+      >
+        Next
+      </button>
+      <button
+        onClick={() => onPageChange(totalPages)}
+        disabled={currentPage === totalPages}
+      >
+        Last
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
